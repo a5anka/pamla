6cb0468a7420ead92b59480262c3e923ff586cb2
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2012-12-06 12:02:15 +0530

    White space adjustments

--- sequential/readmodifywrite.c
+++ sequential/readmodifywrite.c
@@ -30,89 +30,89 @@ float elapsedTime(struct timespec *, struct timespec *, long *, long *);	// prot
 TYPE A[N];
 int main (int argc, char * argv[])
 {
-    int                                 i, j, k, m, mask, stride;
-    TYPE                                value = 1, result=0;
-    int64_t			sum2=0;
-    struct timespec             t1, t2;
-    unsigned long               sec, nsec;
-    float                       elapsed_msec;
+   int                                 i, j, k, m, mask, stride;
+   TYPE                                value = 1, result=0;
+   int64_t			sum2=0;
+   struct timespec             t1, t2;
+   unsigned long               sec, nsec;
+   float                       elapsed_msec;
 
-    for (i=0; i < N; i++) A[i] = i * 1.0;       // init time not counted
+   for (i=0; i < N; i++) A[i] = i * 1.0;       // init time not counted
 
-    GET_TIME(t1);
-    value = 1;
-    mask = N -1 ;	// N is a power of 2
-    stride = DEFAULT_STRIDE;
+   GET_TIME(t1);
+   value = 1;
+   mask = N -1 ;	// N is a power of 2
+   stride = DEFAULT_STRIDE;
 #ifdef LINEAR1
-    for (i = 0; i < REPEAT; i++)
-        for (j = 0; j < N; j++){
-            sum2 += ((j+523) * 253573 ) & mask;
-            A[j] = A[j] + value++;
-        }
+   for (i = 0; i < REPEAT; i++)
+      for (j = 0; j < N; j++){
+         sum2 += ((j+523) * 253573 ) & mask;
+         A[j] = A[j] + value++;
+      }
 #endif
 #ifdef LINEAR2
-    for (i = 0; i < REPEAT; i++)
-        for (j = 0; j < N; j++){
-            sum2 += ((j+523) * 253573 ) & mask + j * stride + j;
-            A[j] = A[j] + value++;
-        }
+   for (i = 0; i < REPEAT; i++)
+      for (j = 0; j < N; j++){
+         sum2 += ((j+523) * 253573 ) & mask + j * stride + j;
+         A[j] = A[j] + value++;
+      }
 #endif
 #ifdef RANDOM1
-    for (i = 0; i < REPEAT; i++)
-        for (j = 0; j < N; j++){
-            k = ((j+523) * 253573 ) & mask;
-            sum2 += k;
-            A[k] = A[k] + value++;
-        }
+   for (i = 0; i < REPEAT; i++)
+      for (j = 0; j < N; j++){
+      k = ((j+523) * 253573 ) & mask;
+      sum2 += k;
+      A[k] = A[k] + value++;
+      }
 #endif
 #ifdef RANDOM2
-    for (i = 0; i < REPEAT; i++)
-        for (j = 0; j < N; j++){
-            k = ((j+523) * 253573 ) & mask;
-            sum2 += k + j * stride + j;
-            A[k] = A[k] + value++;
-        }
+   for (i = 0; i < REPEAT; i++)
+    for (j = 0; j < N; j++){
+       k = ((j+523) * 253573 ) & mask;
+       sum2 += k + j * stride + j;
+       A[k] = A[k] + value++;
+    }
 #endif
 #ifdef STRIDE
-    stride = STRIDE;
-    for (i = 0; i < REPEAT; i++)
-        for (j = 0; j < STRIDE; j++)
-            for (m=0; m < N/STRIDE; m++) {
-                sum2 += ((m+523) * 253573 ) & mask;
-                k = m * STRIDE + j;
-                A[k] = A[k] + value++;
-            }
+   stride = STRIDE;
+   for (i = 0; i < REPEAT; i++)
+    for (j = 0; j < STRIDE; j++)
+       for (m=0; m < N/STRIDE; m++) {
+          sum2 += ((m+523) * 253573 ) & mask;
+          k = m * STRIDE + j;
+          A[k] = A[k] + value++;
+       }
 
 #endif
 
-    result = sum2;
-    GET_TIME(t2);
-        elapsed_msec = elapsedTime(&t1, &t2, &sec, &nsec);
-        printf(MSG1);
+   result = sum2;
+   GET_TIME(t2);
+   elapsed_msec = elapsedTime(&t1, &t2, &sec, &nsec);
+   printf(MSG1);
 #ifdef STRIDE
-        printf("%d:Bad-MA:", STRIDE);
+   printf("%d:Bad-MA:", STRIDE);
 #endif
-        //printf(MSG2, N, elapsed_msec, (int) sec, (int) nsec);
-        printf(MSG2, N, N*sizeof(TYPE)/1024, elapsed_msec);
-        //#ifdef WARMUP
-        //printf(" -- WARM\n");
-        //#else
-        printf("\n");
-        //#endif
+   //printf(MSG2, N, elapsed_msec, (int) sec, (int) nsec);
+   printf(MSG2, N, N*sizeof(TYPE)/1024, elapsed_msec);
+   //#ifdef WARMUP
+   //printf(" -- WARM\n");
+   //#else
+   printf("\n");
+   //#endif
 
-        return 0;
+   return 0;
 }
 //-----------------------------------------------
 float elapsedTime(struct timespec *begin, struct timespec *end, long *sec, long *nsec)
 {
-    if (end->tv_nsec < begin->tv_nsec) {
-        *nsec = 1000000000 - (begin->tv_nsec - end->tv_nsec);
-        *sec  = end->tv_sec - begin->tv_sec -1;
-    }
-        else {
-            *nsec = end->tv_nsec - begin->tv_nsec;
-                *sec  = end->tv_sec - begin->tv_sec;
-        }
-    return (float) (*sec) * 1000 + ((float) (*nsec)) / 1000000;
+   if (end->tv_nsec < begin->tv_nsec) {
+      *nsec = 1000000000 - (begin->tv_nsec - end->tv_nsec);
+      *sec  = end->tv_sec - begin->tv_sec -1;
+   }
+   else {
+      *nsec = end->tv_nsec - begin->tv_nsec;
+      *sec  = end->tv_sec - begin->tv_sec;
+   }
 
+   return (float) (*sec) * 1000 + ((float) (*nsec)) / 1000000;
 }
