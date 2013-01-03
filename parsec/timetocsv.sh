ca46ea6f7f926053bb784ef4176cb95b4cbf41e2
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2013-01-03 14:01:18 +0530

    Minor Improvement

--- parsec/timetocsv.sh
+++ parsec/timetocsv.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-DATAFILE=data/intel-data/parsec-time.out
+DATAFILE="/home/asanka/Dropbox/FYP/Parsec data/Data/streamcluster-time-with_10_pad_O3.out"
 
-sed -e 's/#P/P/g' -e '/^$\|^Swaption\|^PARSEC\|^read\|^Number\|^user\|^sys/d' -e '/([0-9]*,[0-9]*)/d' -e 's/<not counted>/0/g' -e 's/^real	//g' $DATAFILE | \
+sed -e 's/#P/P/g' -e '/^$\|^Swaption\|^PARSEC\|^read\|^Number\|^user\|^sys\|^yuv4mpeg\|^encoded/d' -e '/([0-9]*,[0-9]*)/d' -e 's/<not counted>/0/g' -e 's/^real	//g' "$DATAFILE" | \
 
 awk -F : '{
 if ($1 !~ "PROGRAM.*")
--- parsec/toarff.sh
+++ parsec/toarff.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-DATAFILE=data/intel-data/parsec.out
+DATAFILE=data/intel-data/parsec-with_O_level2.out
 NUMBER_PERFEVENTS=16
 
 LINES=`expr $NUMBER_PERFEVENTS + 1`
 
-echo '@relation performance_data_for_sequential_memory_access
+echo '@relation performance_data_for_parsec_banchmark
 %
 %       16 attributes for each data instance
 %
-@attribute r00c0 numeric
 @attribute r0149 numeric
 @attribute r0151 numeric
 @attribute r02a2 numeric
@@ -25,7 +24,7 @@ echo '@relation performance_data_for_sequential_memory_access
 @attribute r02b8 numeric
 @attribute r04b8 numeric
 @attribute r40cb numeric
-@attribute status {GOOD, BAD-FS, BAD-MA}
+@attribute status {good, badfs, badma}
 @data'
 
 sed -e 's/#P/P/g' -e '/^$\|^Swaption\|^PARSEC\|^read\|^Number\|^yuv4mpeg\|^encoded/d' -e '/([0-9]*,[0-9]*)/d' -e 's/<not counted>/0/g' $DATAFILE | \
@@ -39,4 +38,13 @@ else
 
 sed -e 's/PROGRAM=\|INPUT=\|NTHREADS=//g' | \
  
-awk "ORS=NR%${LINES}?\",\":\"\n\"" | sed 's/$/,?/g' # | cut -d , -f 4-
+awk "ORS=NR%${LINES}?\",\":\"\n\"" | \
+
+awk -F, -v OFS="," '
+{
+    print "% Program: " $1 
+    print "% Input: " $2 
+    print "% Threads: " $3
+    print "% ID: " NR
+    print $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, "?"
+}'
